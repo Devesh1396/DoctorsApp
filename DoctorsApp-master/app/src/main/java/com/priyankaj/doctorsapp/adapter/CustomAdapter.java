06e4ea333314215063513ef27acfc30b2cbc7684
Devesh1396 <devgyani4444@gmail.com>
2018-05-16 22:13:24 +0530

    new changes

---
+++ DoctorsApp-master/app/src/main/java/com/priyankaj/doctorsapp/adapter/CustomAdapter.java
@@ -0,0 +1,79 @@
+package com.priyankaj.doctorsapp.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.priyankaj.doctorsapp.R;
+import com.priyankaj.doctorsapp.model.CategoryDetails;
+import com.priyankaj.doctorsapp.ui.MainActivity;
+import com.priyankaj.doctorsapp.ui.MainActivity2;
+
+import java.util.ArrayList;
+
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    private ArrayList<CategoryDetails.Category> dataSet;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textViewName;
+        TextView textViewVersion;
+        //ImageView imageViewIcon;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            this.textViewName = (TextView) itemView.findViewById(R.id.textViewName);
+            this.textViewVersion = (TextView) itemView.findViewById(R.id.textViewVersion);
+        //    this.imageViewIcon = (ImageView) itemView.findViewById(R.id.imageView);
+
+
+       itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, MainActivity2.class);
+                context.startActivity(intent);
+            }
+        });
+    }
+    }
+
+    public CustomAdapter(ArrayList<CategoryDetails.Category> data) {
+        this.dataSet = data;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                           int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cards_layout, parent, false);
+
+        view.setOnClickListener(MainActivity.myOnClickListener);
+
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int listPosition) {
+
+        TextView textViewName = holder.textViewName;
+        TextView textViewVersion = holder.textViewVersion;
+        //ImageView imageView = holder.imageViewIcon;
+
+        textViewName.setText(dataSet.get(listPosition).getCategory_name());
+        textViewVersion.setText(dataSet.get(listPosition).getStatus());
+       // imageView.setImageResource(dataSet.get(listPosition).getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+}
---
+++ DoctorsApp-master/app/src/main/java/com/priyankaj/doctorsapp/ui/Docdet.java
@@ -0,0 +1,26 @@
+package com.priyankaj.doctorsapp.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.priyankaj.doctorsapp.R;
+
+public class Docdet extends Activity{
+Button book;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.docdet);
+        book=(Button)findViewById(R.id.bookbutton);
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Details.class);
+                startActivity(i);
+            }
+        });
+    }
+}
